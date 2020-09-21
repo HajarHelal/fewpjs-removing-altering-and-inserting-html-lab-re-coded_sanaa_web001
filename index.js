02165418c1e7e0f692d2b4c3f976b231ef2925af
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 13:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
-let newHeader = document.createElement('h1')
+//let newHeader = document.createElement('h1')
 let newHeader = document.createElement('h1#victory')
 newHeader.innerHTML = "Hajar";
